7a043bbba05de0d72d84ebff02f5def3f5b6c56f
Malik Touqeer <touqeeryameen@gmail.com>
2024-12-05 11:25:39 +0500

    commit

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <div className="banner">
+          <div className="left">Malik Touqeer</div>
+          <div className="right">
+            <p>Basti Sharifabad Moza Hussain Abad District Lodhran</p>
+            <p>Open: 05:00 PM - 12:00 AM</p>
+          </div>
+        </div>
+        <div className="banner">
+          <div className="left">
+            <p>Developed By Malik Touqeer</p>
+          </div>
+          <div className="right">
+            <p>All Rights Reserved By Malik Touqeer.</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
